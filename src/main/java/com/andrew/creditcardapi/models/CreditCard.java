8d7a3d217b6a84befd2630b308563a09e8f6d225
andrewy9 <andrew.yang993@gmail.com>
2022-07-17 14:31:33 +1200

    added lombok annotations to allow for constructors and getter/setters. Also added hibernate annotations for entity creation

--- src/main/java/com/andrew/creditcardapi/models/CreditCard.java
+++ src/main/java/com/andrew/creditcardapi/models/CreditCard.java
@@ -1,9 +1,27 @@
 package com.andrew.creditcardapi.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
-
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreditCard {
+    @Id
+    @SequenceGenerator(
+            name = "card_id_sequence",
+            sequenceName = "card_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "users_sequence"
+    )
     private Long id;
     private String full_name;
     private String cardNumber;
