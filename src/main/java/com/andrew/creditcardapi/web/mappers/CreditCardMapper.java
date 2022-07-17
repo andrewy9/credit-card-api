c86730739880d815edb1693007d723b4f0143d33
andrewy9 <andrew.yang993@gmail.com>
2022-07-17 15:37:33 +1200

    added CreditCardMapper

--- src/main/java/com/andrew/creditcardapi/web/mappers/CreditCardMapper.java
+++ src/main/java/com/andrew/creditcardapi/web/mappers/CreditCardMapper.java
@@ -1,2 +1,31 @@
-package com.andrew.creditcardapi.web.mappers;public class CreditCardMapper {
+package com.andrew.creditcardapi.web.mappers;
+
+import com.andrew.creditcardapi.dto.CreditCardRequestDto;
+import com.andrew.creditcardapi.dto.CreditCardResponseDto;
+import com.andrew.creditcardapi.models.CreditCard;
+
+import java.time.LocalDate;
+
+public class CreditCardMapper {
+    public CreditCardResponseDto creditCardResponseDto(CreditCard creditCard) {
+        Long id = creditCard.getId();
+        String fullName = creditCard.getFullName();
+        String cardNumber = creditCard.getCardNumber();
+        String cvc = creditCard.getCvc();
+        LocalDate expiryDate = creditCard.getExpiryDate();
+
+        return new CreditCardResponseDto(id, fullName, cardNumber, cvc, expiryDate);
+    }
+
+    public CreditCard toEntity(CreditCardRequestDto requestDto) {
+        CreditCard creditCard = new CreditCard(
+                null,
+                requestDto.getFullName(),
+                requestDto.getCardNumber(),
+                requestDto.getCvc(),
+                requestDto.getExpiryDate()
+        );
+
+        return creditCard;
+    }
 }
