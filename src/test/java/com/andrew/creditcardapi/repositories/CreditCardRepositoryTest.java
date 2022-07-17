dfab3b2fb58e538ba9183d06f26d464e74df1740
andrewy9 <andrew.yang993@gmail.com>
2022-07-17 16:46:08 +1200

    basic repository unit testing added

---
+++ src/test/java/com/andrew/creditcardapi/repositories/CreditCardRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.andrew.creditcardapi.repositories;
+
+import com.andrew.creditcardapi.models.CreditCard;
+import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CreditCardRepositoryTest {
+
+    @Autowired
+    private static CreditCardRepository creditCardRepoUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        creditCardRepoUnderTest.deleteAll();
+    }
+
+    @Test
+    void canFindByCardNumber() {
+        //given
+        String cardNumber = "1111222233334444";
+        CreditCard creditCard = new CreditCard(
+                10L,
+                "John Snow",
+                cardNumber,
+                "123",
+                LocalDate.now()
+        );
+        creditCardRepoUnderTest.save(creditCard);
+        //when
+        boolean expected = creditCardRepoUnderTest.findByCardNumber(cardNumber).isPresent();
+        //then
+        assertThat(expected).isTrue();
+    }
+
+    @Test
+    void canCheckIfCardNumberDoesNotExist() {
+        //given
+        String cardNumber = "1111222233334444";
+        //when
+        boolean expected = creditCardRepoUnderTest.findByCardNumber(cardNumber).isPresent();
+        //then
+        assertThat(expected).isFalse();
+    }
+}
